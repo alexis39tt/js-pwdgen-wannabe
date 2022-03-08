69422c79cba887b2b8cdbd70805d246dba0e5eee
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-08 15:15:56 +0100

    added random number generator

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,8 @@ function Password(){
     let nome = document.getElementById('nome').value;
     let cognome = document.getElementById('cognome').value;
     let colore = document.getElementById('colore').value;
-
-    console.log(nome);
+    let result = document.getElementById('password');
+    let numero = Math.round(Math.random() * 1000);
+    
+    result.innerHTML = `La tua password è: ${nome}${cognome}${colore}${numero}`;
 }
