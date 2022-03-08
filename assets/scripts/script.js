22232879ccc0c82a10fea09c7e11a0bd4f73f39e
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-08 13:52:50 +0100

    added html and css style

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+function Password(){
+    let nome = document.getElementById('nome').value;
+    let cognome = document.getElementById('cognome').value;
+    let colore = document.getElementById('colore').value;
+
+    console.log(nome);
+}
